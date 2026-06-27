7b764d3f82e39003d15a153a1bb3089fa617e50b
seyed mohammad mahdi hosseini <114837509+seyedmh9@users.noreply.github.com>
2026-06-27 04:43:39 +0330

    Delete vpn-sub-app directory

--- vpn-sub-app/app/src/main/java/com/example/vpnsub/Config.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vpnsub
-
-data class Config(
-    val name: String,
-    val link: String
-)
--- vpn-sub-app/app/src/main/java/com/example/vpnsub/Subscription.kt
+++
@@ -1,6 +0,0 @@
-package com.example.vpnsub
-
-data class Subscription(
-    val name: String,
-    val url: String
-)
